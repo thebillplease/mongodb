daeb4c3d35f508dd27341cbdcdaf902b3debf8c1
thebillplease <billplease@gmail.com>
2015-06-18 23:52:15 -0400

    Update dbschema.js

--- dbschema.js
+++ dbschema.js
@@ -3,8 +3,8 @@
 "hubId" 		: 93230,
 "sensorId"		: 2,
 "temp"			: 4,
+"session"		: 5555,
 "category"		: 1,
-"dateEntered"		: ISODate("2014-03-01T08:00:00Z"), 
 "vcc1"			: 98,
 "vcc2"			: 97,
 "seq"			: 45,
@@ -13,7 +13,7 @@
 
 
 // Stored Content
-"_id"		: 	uid, 
+"_id"		: 	session, 
 "end"		:	ISODate("2014-03-21T12:18:12.648Z"),
 "start"		:	ISODate("2014-03-21T12:16:39.047Z"), // enter date
 "fBy"		:	ISODate("2014-03-21T12:20:39.047Z"), // freshest by date. calculated on an hourly basis. 
@@ -29,7 +29,7 @@
 "_id"	:	ObjectId("0349dasdf932rkasf"),
 "hubId"	:	hubId,	
 "senId"	:	senId,
-"uids"	:	{ uid, uid, uid, uid },
+"uids"	:	{ session, session, session, session },
 "stat"	:	"on",
 "bat"	:	97,
 "last"	:	ISODate("2014-03-21T12:18:12.648Z")
@@ -46,10 +46,10 @@
 //Sensor schema. A single sensor will update every minute. Sensor object tracks 1hr into 60 1 min. chunks.  
 // based on this http://blog.mongodb.org/post/65517193370/schema-design-for-time-series-data-in-mongodb
 // https://www.mongodb.com/presentations/mongodb-time-series-data-part-1-setting-stage-sensor-management?_ga=1.96703583.1535413078.1434404051
-"_id"	: 	ObjectId("0349dasdf932rkasf"),
-"hubId"	: 123,
-"senId"	: 123,
-"uid"	: 5555, // unique id generated every time sensor is turned on.
+"_id"		: 	ObjectId("0349dasdf932rkasf"),
+"hubId"		: 123,
+"senId"		: 123,
+"session"	: 5555, // unique id generated every time sensor is turned on.
 "val"	: [
 	{
             "timestamp" : ISODate("2014-03-21T12:16:44.594Z"),
