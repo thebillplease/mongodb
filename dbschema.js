5cda273317d8add1b526a3d53972d370dfd1b3ad
thebillplease <billplease@gmail.com>
2015-06-18 23:49:00 -0400

    Update dbschema.js

--- dbschema.js
+++ dbschema.js
@@ -1,10 +1,10 @@
 // POST Values
 {
-"hubId" : 93230,
+"hubId" 		: 93230,
 "sensorId"		: 2,
 "temp"			: 4,
 "category"		: 1,
-"dateEntered"	: ISODate("2014-03-01T08:00:00Z"), 
+"dateEntered"		: ISODate("2014-03-01T08:00:00Z"), 
 "vcc1"			: 98,
 "vcc2"			: 97,
 "seq"			: 45,
@@ -13,32 +13,34 @@
 
 
 // Stored Content
-"_id"	: 	senId, 
-"end"	:	ISODate("2014-03-21T12:18:12.648Z"),
-"start"	:	ISODate("2014-03-21T12:16:39.047Z"),
-"fBy"	:	ISODate("2014-03-21T12:20:39.047Z"), // freshest by date. calculated on an hourly basis. 
-"cat"	:	1, 
-"cLabel"	: "Chicken ricotta with lime", // user entered label
-"notes"	: 	"Remember to add salt", // user entered notes
-"avgT"	:	 7, // avg. temp from start till now
-"stat"	:	"1",
-"tags"	: 	{catname, ricotta, lime}
+"_id"		: 	uid, 
+"end"		:	ISODate("2014-03-21T12:18:12.648Z"),
+"start"		:	ISODate("2014-03-21T12:16:39.047Z"), // enter date
+"fBy"		:	ISODate("2014-03-21T12:20:39.047Z"), // freshest by date. calculated on an hourly basis. 
+"cat"		:	1, 
+"cLabel"	: 	"Chicken ricotta with lime", // user entered label
+"notes"		: 	"Remember to add salt", // user entered notes
+"avgT"		:	 7, // avg. temp from start till now
+"stat"		:	"1",
+"tags"		: 	{catname, ricotta, lime}
 
 
 // Sensors
 "_id"	:	ObjectId("0349dasdf932rkasf"),
-"hubId":	hubId,	
+"hubId"	:	hubId,	
 "senId"	:	senId,
+"uids"	:	{ uid, uid, uid, uid },
 "stat"	:	"on",
 "bat"	:	97,
 "last"	:	ISODate("2014-03-21T12:18:12.648Z")
 
 
 // Hubs
-"_id"	: 
-"userId":
+"_id"	: 	ObjectId("0349dasdf932rkasf"),
+"hubId"	:	1234, 
+"userId":	
 "vs"	:
-"last"	:
+"last"	:	ISODate("2014-03-21T12:16:44.594Z")
 
 
 //Sensor schema. A single sensor will update every minute. Sensor object tracks 1hr into 60 1 min. chunks.  
@@ -47,6 +49,7 @@
 "_id"	: 	ObjectId("0349dasdf932rkasf"),
 "hubId"	: 123,
 "senId"	: 123,
+"uid"	: 5555, // unique id generated every time sensor is turned on.
 "val"	: [
 	{
             "timestamp" : ISODate("2014-03-21T12:16:44.594Z"),
@@ -94,6 +97,7 @@
 
 
 //person
+"_id"		: 	ObjectId("0349dasdf932rkasf"),
 "username"	: 'username',
 "name"	: 'first last',
 "password"	: 'password',
